a6eb0b6763efabb64547c30d43929389ce2e2d06
LeonzeT <leonardzuiga@gmail.com>
2026-03-23 03:12:21 +0800

    Add files via upload

---
+++ Temp/bitNode-multipliers.txt.js
@@ -0,0 +1,5 @@
+import { jsonReplacer } from 'helpers.js'
+export async function main(ns) { let r;try{r=JSON.stringify(
+    ns.getBitNodeMultipliers()
+, jsonReplacer);}catch(e){r="ERROR: "+(typeof e=='string'?e:e?.message??JSON.stringify(e));}
+const f="/Temp/bitNode-multipliers.txt"; if(ns.read(f)!==r) ns.write(f,r,'w') }
